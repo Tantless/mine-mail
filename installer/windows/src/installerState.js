c5ffc9ce193ef2a770fb61434364c832d8edcdb5
Tantless <tantless@163.com>
2026-07-31 11:31:50 +0800

    release: 发布 v0.2.1

--- installer/windows/src/installerState.js
+++ installer/windows/src/installerState.js
@@ -19,7 +19,7 @@ export function stepTone(state, index) {
 
 export function defaultPreviewInfo() {
   return {
-    version: "0.2.0",
+    version: "0.2.1",
     defaultInstallDir: "C:\\Users\\You\\AppData\\Local\\Mine Mail",
     payloadAvailable: true,
   };
--- installer/windows/src/installerState.test.js
+++ installer/windows/src/installerState.test.js
@@ -22,7 +22,7 @@ describe("installer state presentation", () => {
 
   it("provides a complete browser-preview fallback", () => {
     expect(defaultPreviewInfo()).toMatchObject({
-      version: "0.2.0",
+      version: "0.2.1",
       payloadAvailable: true,
     });
   });
