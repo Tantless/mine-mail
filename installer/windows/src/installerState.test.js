df5fc562763ade8685fa37b87c32a9bf02be5a84
Tantless <tantless@163.com>
2026-08-10 22:39:17 +0800

    release: 发布 v1.3.4

--- installer/windows/src/installerState.test.js
+++ installer/windows/src/installerState.test.js
@@ -22,7 +22,7 @@ describe("installer state presentation", () => {
 
   it("provides a complete browser-preview fallback", () => {
     expect(defaultPreviewInfo()).toMatchObject({
-      version: "1.3.3",
+      version: "1.3.4",
       payloadAvailable: true,
     });
   });
